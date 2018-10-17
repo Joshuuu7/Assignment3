ec69d812640ab543f1301df24400b4de0f93cbc8
Joshua Flores <joshuaaaronflores@gmail.com>
2018-10-17 00:48:55 -0500

    Got rid of all errors and set up the storyboard.

---
+++ Assignment3/CoreDataStack.swift
@@ -0,0 +1,70 @@
+//
+//  CoreDataStack.swift
+//  Assignment3
+//
+//  Created by Joshua Aaron Flores Stavedahl on 10/17/18.
+//  Copyright © 2018 Northern Illinois University. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    private let modelName: String
+    
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+    
+    lazy var managedContext: NSManagedObjectContext = {
+        return self.persistentContainer.viewContext
+    }()
+    
+    // MARK: - Core Data stack
+    
+    private lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+         */
+        let container = NSPersistentContainer(name: self.modelName)
+        container.loadPersistentStores {
+            (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        guard managedContext.hasChanges else {
+            return
+        }
+        
+        do {
+            try managedContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nserror = error as NSError
+            print("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+    }
+}
