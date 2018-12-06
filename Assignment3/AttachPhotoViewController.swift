d346189bc297422a412bd0632f164fba71136369
Joshua Flores <joshuaaaronflores@gmail.com>
2018-12-05 22:28:22 -0600

    Access to camera and photo library now works.

---
+++ Assignment3/AttachPhotoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AttachPhotoViewController.swift
+//  Assignment3
+//
+//  Created by Joshua Aaron Flores Stavedahl on 12/5/18.
+//  Copyright © 2018 Northern Illinois University. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MobileCoreServices
+import AVFoundation
+import Photos
+
+class AttachPhotoViewController: UIViewController {
+    
+    
+}
--- Assignment3/DetailViewController.swift
+++ Assignment3/DetailViewController.swift
@@ -19,6 +19,9 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var ratingLabel: UILabel!
     @IBOutlet weak var releaseYearLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var photoButton: UIButton!
+    
+    let image = UIImage(named: "camera.png")
     
     func configureView() {
         if let detail = detailItem {
@@ -30,12 +33,54 @@ class DetailViewController: UIViewController {
             }
             if let label = ratingLabel {
                 label.textColor = UIColor.green
-                label.text = "Rating: " + detail.rating!
+                label.text = "Rating: " + detail.rating! + " / 5"
             }
             if let label = releaseYearLabel {
                 label.textColor = UIColor.green
                 label.text = "Release year: " + detail.releaseYear!
             }
+            /*if let imageView = self.imageView {
+                //(image: UIImage?) in
+                imageView.image = image
+            }*/
+        }
+    }
+    @IBAction func photoClick(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "toPhotoViewController", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        /*if segue.identifier == "toPhotoViewController" {
+            guard segue.destination is AttachPhotoViewController else {
+                return
+            }
+        }*/
+        showAlert()
+    }
+    
+    func showAlert() {
+        
+        let alert = UIAlertController(title: "", message: "", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action: UIAlertAction) in
+            self.getImage(fromSourceType: .camera)
+        }))
+        alert.addAction(UIAlertAction(title: "Photo Album", style: .default, handler: {(action: UIAlertAction) in
+            self.getImage(fromSourceType: .photoLibrary)
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    //get image from source type
+    func getImage(fromSourceType sourceType: UIImagePickerControllerSourceType) {
+        
+        //Check is source type available
+        if UIImagePickerController.isSourceTypeAvailable(sourceType) {
+            
+            let imagePickerController = UIImagePickerController()
+            //imagePickerController.delegate = self
+            imagePickerController.sourceType = sourceType
+            self.present(imagePickerController, animated: true, completion: nil)
         }
     }
     
@@ -43,6 +88,8 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         configureView()
+        
+        view.endEditing(true)
     }
     
     override func didReceiveMemoryWarning() {
